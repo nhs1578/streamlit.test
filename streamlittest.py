26ce681b94d01e895856bc45701581cf557d0cd1
nhs1578 <jch0855@naver.com>
2025-02-13 16:06:33 +0900

    Update streamlittest.py

--- streamlittest.py
+++ streamlittest.py
@@ -1,4 +1,30 @@
 import streamlit as st
+import panas as pd
+import matplotlib.pyplot as plt
+
+st.title("st.write() 활용 예시")
+
+# 1. 텍스트 출력 (Markdown 지원)
+st.write("## 마크다운 제목")
+st.write("* 강조된 텍스트 *")
+
+# 2. 데이터프레임 출력
+df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+st.write("### 데이터프레임")
+st.write(df)
+
+# 3. 그래프 출력 (Matplotlib)
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3], [1, 4, 2])
+st.write("### Matplotlib 그래프")
+st.pyplot(fig)
+
+# 4. 이미지 출력
+st.write("### 이미지")
+st.image("https://streamlit.io/images/brand/streamlit-logo-horizontal.svg", width=300)
+
+
+
 
 st.write("# test")
 st.header("header test")
